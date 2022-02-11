fb8f00e5dd49b06144605deff879cc790aa36719
max-las <lasserre.maxime@yahoo.fr>
2022-02-11 12:57:54 +0100

    begin dialogs

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,12 @@
 <script>
-	let name = "Regards";
+	import Dialog from "./components/Dialog.svelte";
+	import { dialogs } from "./lib/dialogs.js";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Dialog first={dialogs.begin} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/components/Dialog.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let first;
+  let current = first;
+</script>
+
+<div class="dialog" on:click={() => {current = current.next()}}>
+  <img src="/img/decoration_left.png" alt="" />
+
+  <div class="content">
+    <p>{current.text()}</p>
+    <img src="/img/arrow.png" alt="" />
+  </div>
+
+  <img src="/img/decoration_right.png" alt="" />
+</div>
+
+<style lang="scss">
+  .dialog {
+    background-color: rgba(1,1,1,0.3);
+    width: 100vw;
+    height: 95px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+
+  .content{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+    p {
+      color: white;
+    }
+  }
+</style>
