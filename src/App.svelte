ca8c04371c9892f49b800f6311325695f94e221a
max-las <lasserre.maxime@yahoo.fr>
2022-02-14 17:53:26 +0100

    progress

--- src/App.svelte
+++ src/App.svelte
@@ -4,24 +4,45 @@
 	import dialogs from './data/dialogs.json';
 	import coordinates from './data/coordinates.json'
 	import { currentDialogIndex } from "./lib/stores.js";
+	import { wait } from "./lib/helpers.js";
 
 	let mainPictureBg;
 	let nextPictureSafe = pictures[dialogs[$currentDialogIndex+1].picture];
 	let pictureSafe = pictures[dialogs[$currentDialogIndex].picture];
 
 	let clickCoords = null;
+	let zoomCoords = null;
+	let zoomCoordsPrev = null;
+	let opacityTransitionOn = false;
 
 	$: {
 		if($currentDialogIndex){
-			pictureTransition();
+			if(pictures[dialogs[$currentDialogIndex].picture] !== pictureSafe){
+				pictureTransition();
+			}else if(zoomCoords || zoomCoordsPrev){
+				try {
+					mainPictureBg.style.animation = 'none';
+					mainPictureBg.offsetHeight; /* trigger reflow */
+					mainPictureBg.style.animation = null; 
+				} catch(err) {}
+			}
 		}
 	}
 
 	$: {
-		if(dialogs[$currentDialogIndex].click !== "undefined"){
+		if(typeof dialogs[$currentDialogIndex].click !== "undefined"){
 			clickCoords = coordinates[dialogs[$currentDialogIndex].click];
 		}else{
 			clickCoords = null;
+		}
+	}
+
+	$: {
+		if(typeof dialogs[$currentDialogIndex].zoom !== "undefined"){
+			zoomCoordsPrev = zoomCoords;
+			zoomCoords = coordinates[dialogs[$currentDialogIndex].zoom];
+		}else{
+			zoomCoords = null;
 		}
 	}
 
@@ -29,28 +50,42 @@
 		$currentDialogIndex += 1;
 	}
 
-	function pictureTransition(){
+	async function pictureTransition(){
 		try {
+			opacityTransitionOn = true;
 			mainPictureBg.style.opacity = 0;
-			setTimeout(() => {
-				pictureSafe = pictures[dialogs[$currentDialogIndex].picture];
-				mainPictureBg.style.opacity = null;
-				setTimeout(() => {
-					let nextDialog = dialogs[$currentDialogIndex+1];
-					if(nextDialog){
-						if(nextDialog.picture){
-							nextPictureSafe = pictures[nextDialog.picture];
-						}
-					}
-				}, 1000);
-			}, 1000);
+			await wait(1000);
+			opacityTransitionOn = false;
+			pictureSafe = pictures[dialogs[$currentDialogIndex].picture];
+			await wait(100); // fix flash on transition
+			mainPictureBg.style.opacity = null;
+			let nextDialog = dialogs[$currentDialogIndex+1];
+			if(nextDialog){
+				if(nextDialog.picture){
+					nextPictureSafe = pictures[nextDialog.picture];
+				}
+			}
 		} catch (err) {}
 	}
 </script>
 
 <main>
 	<div class="pictureBg" style="background-image: url('/img/pictures/{nextPictureSafe}')"></div>
-	<div class="pictureBg" bind:this={mainPictureBg} style="background-image: url('/img/pictures/{pictureSafe}')"></div>
+	<div 
+		class="pictureBg"
+		class:opacityTransition="{opacityTransitionOn}"
+		bind:this={mainPictureBg}
+		style="
+			background-image: url('/img/pictures/{pictureSafe}'); 
+			{zoomCoords || zoomCoordsPrev ? `
+				--bgpos: ${zoomCoords ? zoomCoords.backgroundPosition : "center" };
+				--bgsize: ${zoomCoords ? zoomCoords.backgroundSize : "100%"};
+				--bgpos-prev: ${zoomCoordsPrev ? zoomCoordsPrev.backgroundPosition : "center"};
+				--bgsize-prev: ${zoomCoordsPrev ? zoomCoordsPrev.backgroundSize : "100%"};
+				animation: zoom 2s forwards;
+			` : "" }
+		"
+	></div>
 	{#if dialogs[$currentDialogIndex].text !== ""}
 		<Dialog first={$currentDialogIndex} />
 	{:else if clickCoords}
@@ -72,11 +107,14 @@
 		top: 0;
 		left: 0;
 		background-position: center;
-		background-size: cover;
-		transition: opacity 1s;
+		background-size: 100%;
+		&.opacityTransition {
+			transition: opacity 1s;
+		}
 	}
 
 	.clickDiv {
 		position: absolute;
+		border: 2px solid red;
 	}
 </style>
---
+++ src/lib/helpers.js
@@ -0,0 +1,5 @@
+export const wait = (ms) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, ms);
+});
