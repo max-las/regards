814bc45a390c2223feedcfc43007076e963bf4ea
max-las <lasserre.maxime@yahoo.fr>
2022-02-14 10:49:32 +0100

    click interactions

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 export const picture = writable("museum_inside.jpg");
 export const nextPicture = writable("children.jpg");
+export const currentDialogIndex = writable(0);
